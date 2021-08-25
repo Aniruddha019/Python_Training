dfadd2c09a74ce87b9620ae4716bca4598b33643
Aniruddha Sharma <aniruddha.s@perfios.com>
2021-08-25 12:50:57 +0530

    Added Python basics files

--- Batches/2021-Aug-22/SanjuktaDas/Exercise_001_Python_basics.py
+++ Batches/2021-Aug-22/SanjuktaDas/Exercise_001_Python_basics.py
@@ -1,5 +1,5 @@
 """
 Practice using the Python interpreter as a calculator:
-1. The volume of a sphere with radius r is 3 4 πr 3 . What is the volume of a sphere with radius 5?
+1. The volume of a sphere with radius r is 4/3 * π(r^3) . What is the volume of a sphere with radius 5?
 """
 # Write your program below
