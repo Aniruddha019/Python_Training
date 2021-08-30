69e54a062204451db1a5dc5674d934809d20d395
ananyasaikia1996 <ananyasaikia1996@gmail.com>
2021-08-30 19:11:32 +0530

    added second exerise

--- Batches/2021-Aug-22/AnanyaSaikia/Exercise_003_IfElse_SquareOrNot.py
+++ Batches/2021-Aug-22/AnanyaSaikia/Exercise_003_IfElse_SquareOrNot.py
@@ -1,3 +1,12 @@
 """
 Take values of length and breadth of a rectangle from user and check if it is square or not.
 """
+length=input("enter the length=")
+breadth=input("enter the breadth=")
+if length==breadth:
+    print("the object is a square")
+else:
+    print("the object is not a square")
+
+
+
